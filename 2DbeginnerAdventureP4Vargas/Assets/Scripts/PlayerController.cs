5274d2470e3e10ac49066af63c51eebb83fe1488
cogito17 <729426@student.dusd.net>
2024-10-22 11:45:34 -0700

    movement & tiles
    created a script that allows me to move my character & i created some tiles.

--- 2DbeginnerAdventureP4Vargas/Assets/Scripts/PlayerController.cs
+++ 2DbeginnerAdventureP4Vargas/Assets/Scripts/PlayerController.cs
@@ -7,14 +7,18 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+  
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 4.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 4.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
