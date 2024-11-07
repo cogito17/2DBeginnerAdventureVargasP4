c27356326c8ba4cae6179996860717fe58a23623
cogito17 <729426@student.dusd.net>
2024-11-07 11:40:02 -0800

    move it
    .

--- 2DbeginnerAdventureP4Vargas/Assets/Scripts/PlayerController.cs
+++ 2DbeginnerAdventureP4Vargas/Assets/Scripts/PlayerController.cs
@@ -17,18 +17,52 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
+
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>(); 
 
         currentHealth = maxHealth;
         currentHealth = 1;
-  
+
     }
 
     // Update is called once per frame
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
+
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+        Debug.Log(horizontal);
+
+        float vertical = 0.0f;
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+        Debug.Log(vertical);
+
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f;
+        transform.position = position;
+
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
+        transform.position = position;
     }
 
     void FixedUpdate()
